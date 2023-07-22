62cf5ce18cb5715be33ee7a548e0a74019e4e052
VERCHERE Rémi <remi.verchere2@gmail.com>
2023-07-22 14:58:36 +0200

    add test

--- eanCheck_test.go
+++ eanCheck_test.go
@@ -4,10 +4,23 @@ import (
 	"testing"
 )
 
-func TestICorrectEan_noerror(t *testing.T) {
+func TestIsCorrectEan(t *testing.T) {
 	checkValue := "3666154117284"
 	test := isCorrectEan(checkValue)
 	if !test {
 		t.Fatalf(`isCorrectEan("%v") should return true but have %t`, checkValue, test)
+	}
+	checkValue2 := "12345670"
+	test2 := isCorrectEan(checkValue)
+	if !test {
+		t.Fatalf(`isCorrectEan("%v") should return true but have %t`, checkValue2, test2)
+	}
+}
+
+func TestCalculateDigitCheck(t *testing.T) {
+	checkValue := "366615411728"
+	test := calculateDigitCheck(checkValue)
+	if test != "4" {
+		t.Fatalf(`calculateDigitCheck("%v") should return 4 but have %v`, checkValue, test)
 	}
 }
